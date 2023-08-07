4e2d3827df71c6352f39ed91608dc2defad40a93
Moch Syafrizal Azhar <111201912014@mhs.dinus.ac.id>
2023-08-08 03:12:30 +0700

    update

--- sales_tracking.sql
+++ sales_tracking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2023 at 06:33 PM
+-- Generation Time: Aug 07, 2023 at 08:37 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,29 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `sales_tracking`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `version` varchar(255) NOT NULL,
-  `class` varchar(255) NOT NULL,
-  `group` varchar(255) NOT NULL,
-  `namespace` varchar(255) NOT NULL,
-  `time` int(11) NOT NULL,
-  `batch` int(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(1, '2023-07-21-054543', 'App\\Database\\Migrations\\TabelMaps', 'default', 'App', 1689918390, 1);
 
 -- --------------------------------------------------------
 
@@ -65,8 +42,10 @@ CREATE TABLE `tbl_dbmaps` (
 --
 
 INSERT INTO `tbl_dbmaps` (`id_maps`, `nama_maps`, `no_telpon`, `alamat`, `latitude`, `longitude`, `deskripsi`) VALUES
-(1, 'Universitas Dian Nuswantoro', '0243517261', 'Jl. Imam Bonjol No.207, Pendrikan Kidul, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50131', '-6.982376092662107', '110.4091363963356', 'Universitas Swasta'),
-(2, 'Kost Trimo Ayem', '085623984512', 'Gg. VI No.2016, Lamper Tengah, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50248', '-7.0043806880192685', '110.44351435394897', 'Tempat Kost yang berada di Lamper Tengah');
+(3, 'Toko A', '0821201012', 'Komplek Masjid Agung Kauman, 2CHF+528, Bangunharjo, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50139, Indonesia', '-6.972198213119912', '110.42265522962933', 'Toko pemilik Bapak Suratno penjual Rokok'),
+(4, 'Toko B', '08560201023', 'Jl. Mayor Jend. D.I. Panjaitan No.9, Miroto, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50134, Indonesia', '-6.985786775556099', '110.42063820845013', 'Toko milik Bu Sutarmi penjual toko kelontong'),
+(5, 'Smart Printer', '0854644887922', 'Jl. Mugas Dalam IX No.5, Mugassari, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50249, Indonesia', '-6.99166509090616', '110.41535962110882', 'Toko printer bapak Jono customer tinta'),
+(6, 'Karangan Bunga', '08879945621', 'Jl. Suyudono No.158, Barusari, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50245, Indonesia', '-6.989407485784443', '110.4042445469511', 'Toko bunga Bu Sri customer kayu');
 
 -- --------------------------------------------------------
 
@@ -91,7 +70,9 @@ CREATE TABLE `tbl_user` (
 
 INSERT INTO `tbl_user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
 (1, 'Moch Syafrizal Azhar', 'mochsyafrizalazhar@gmail.com', 'default.png', '$2y$10$9jIlcwHwPU8VaQ2t.sNSvOQMll9lNaorzpYsJOAv6Vs/Z4d4tg6QC', 1, 1, 1691166752),
-(2, 'Doddy Sumargo', 'hahaha250901@gmail.com', 'default.png', '$2y$10$oCTIr83XbR1hTXvVbw/67eP/Lf5jdbxiPQa1FntUPCTgunCj.1va6', 2, 1, 1691167350);
+(2, 'Doddy Sumargo', 'hahaha250901@gmail.com', 'default.png', '$2y$10$oCTIr83XbR1hTXvVbw/67eP/Lf5jdbxiPQa1FntUPCTgunCj.1va6', 2, 1, 1691167350),
+(3, 'Sukarni Tejo', 'tejo@gmail.com', 'default.png', '$2y$10$uoVUzi9vaRtNzk9mUKxba.6xmC89HzrecG96LltlkK8mUOeLCDWwO', 2, 1, 1691346760),
+(4, 'Gerry Hendra', 'gerry@gmail.com', 'default.png', '$2y$10$YX0xLzA.zMlOYLLArqb8.OUCrRcLFYUIjLiPtKdMXgovc2/9AQnp.', 2, 1, 1691347797);
 
 -- --------------------------------------------------------
 
@@ -132,7 +113,7 @@ CREATE TABLE `user_menu` (
 --
 
 INSERT INTO `user_menu` (`id`, `menu`) VALUES
-(1, 'Main'),
+(1, 'Main Menu'),
 (2, 'Tugas'),
 (3, 'Database');
 
@@ -175,21 +156,15 @@ CREATE TABLE `user_sub_menu` (
 --
 
 INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
-(1, 1, 'Dashboard', '', 'fas fa-fire', 1),
+(1, 1, 'Dashboard', 'dashboard', 'fas fa-fire', 1),
 (2, 1, 'Google Maps', 'gmaps', 'fas fa-map-marker-alt', 1),
-(3, 2, 'Tugas', 'tugas', 'fas fa-tasks', 1),
 (4, 3, 'Maps', 'dbmaps', 'fas fa-map-marked-alt', 1),
-(5, 3, 'Account', 'dbaccount', 'fas fa-users-cog', 1);
+(5, 3, 'Account', 'dbaccount', 'fas fa-users-cog', 1),
+(6, 2, 'Tugas', 'tugas', 'fas fa-tasks', 1);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_dbmaps`
@@ -232,22 +207,16 @@ ALTER TABLE `user_sub_menu`
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `tbl_dbmaps`
 --
 ALTER TABLE `tbl_dbmaps`
-  MODIFY `id_maps` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_maps` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
@@ -271,7 +240,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
